20f5ed93e80f0e000c8b54fea338362ea080ac44
reehmendes <rehmeendes1@gmail.com>
2021-11-18 09:52:08 -0300

    Primeiro Commit
    Envio da API sem a parte do frontEnd, com os controladores de Fornecedor e Produto adicionados.

---
+++ MinhaApiCompleta/src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,115 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using DevIO.Business.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IFornecedorService _fornecedorService;
+        private readonly IEnderecoRepository _enderecoRepository;
+        private readonly IMapper _mapper;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository,
+                                     IMapper mapper,
+                                     IFornecedorService fornecedorService,
+                                     INotificador notificador,
+                                     IEnderecoRepository enderecoRepository) : base(notificador)
+        {
+            _fornecedorRepository = fornecedorRepository;
+            _mapper = mapper;
+            _fornecedorService = fornecedorService;
+            _enderecoRepository = enderecoRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
+        {
+            var fornecedor = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
+            return fornecedor;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> ObterTodosPorId(Guid id)
+        {
+            var fornecedor = await ObterFornecedorProdutosEndereco(id);
+            if (fornecedor == null) return BadRequest();
+            return fornecedor;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<FornecedorViewModel>> Adicionar(FornecedorViewModel fornecedorViewModel)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+            await _fornecedorService.Adicionar(_mapper.Map<Fornecedor>(fornecedorViewModel));
+
+            return CustomResponse(fornecedorViewModel); //sempre retornar a model para não mostrar dados sensíveis            
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> Atualizar(Guid id, FornecedorViewModel fornecedorViewModel)
+        {
+            if (id != fornecedorViewModel.Id) 
+            {
+                NotificarErro("O Id informado não é o mesmo que foi passado na query");
+                return CustomResponse(fornecedorViewModel);
+            }
+
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+            await _fornecedorService.Atualizar(_mapper.Map<Fornecedor>(fornecedorViewModel));
+
+            return CustomResponse(fornecedorViewModel);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> Excluir(Guid id)
+        {
+            var fornecedorViewModel = await ObterFornecedorEndereco(id);
+            if (fornecedorViewModel == null) return NotFound();
+
+            await _fornecedorService.Remover(id);
+            return CustomResponse(fornecedorViewModel);
+        }
+
+        [HttpGet("endereco/{id:guid}")]
+        public async Task<EnderecoViewModel> ObterEnderecoPorId(Guid id)
+        {
+            return _mapper.Map<EnderecoViewModel>(await _enderecoRepository.ObterPorId(id));
+        }
+
+        [HttpPut("endereco/{id:guid}")]
+        public async Task<IActionResult> AtualizarEndereco(Guid id, EnderecoViewModel enderecoViewModel)
+        {
+            if (id != enderecoViewModel.Id)
+            {
+                NotificarErro("O id informado não é o mesmo que foi passado na query");
+                return CustomResponse(enderecoViewModel);
+            }
+
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            await _fornecedorService.AtualizarEndereco(_mapper.Map<Endereco>(enderecoViewModel));
+
+            return CustomResponse(enderecoViewModel);
+        }
+
+        private async Task<FornecedorViewModel> ObterFornecedorProdutosEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorProdutosEndereco(id));
+        }
+
+
+        private async Task<FornecedorViewModel> ObterFornecedorEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorEndereco(id));
+        }
+    }
+}
---
+++ MinhaApiCompleta/src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,70 @@
+﻿using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Linq;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        private readonly INotificador _notificador;
+
+        protected MainController(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        protected bool OperacaoValida()
+        {
+            return !_notificador.TemNotificacao(); //se o notifidicador não possuir nenhuma notificação
+        }
+
+
+        //valida se existe algum tipo de notificação
+        protected ActionResult CustomResponse(object resultado = null)
+        {
+            if (OperacaoValida())
+            {
+                return Ok(new
+                {
+                    sucesso = true,
+                    data = resultado
+                });
+            }
+
+            return BadRequest(new { 
+                sucesso = false,
+                erros = _notificador.ObterNotificacoes().Select(m => m.Mensagem) //cria uma lista de mensagens de erro
+            });
+        }
+
+        //sem validar a regra de negócio, verificando apenas com a modelState
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            if (!modelState.IsValid) NotificarErroModelInvalida(modelState);
+            return CustomResponse();
+        }
+        
+        protected void NotificarErroModelInvalida(ModelStateDictionary modelState)
+        {
+            //selecionar os error
+            var erros = modelState.Values.SelectMany(e => e.Errors);
+            foreach(var erro in erros)
+            {
+                var errorMsg = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+                NotificarErro(errorMsg);
+            }
+        }
+
+        protected void NotificarErro(string mensagem)
+        {
+            _notificador.Handle(new Notificacao(mensagem));
+        }
+    }
+}
+
+//validação de notificações de erro
+//validação da Modelstate
+//validação da operação de negócios
